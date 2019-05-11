2949e948c7734045cc6811ab14384d872e621ddd
Dmitriy <pershikovdmitriy@yandex.ru>
2019-05-11 12:54:16 +0300

    Updates

---
+++ precache-manifest.970126a1e1ab3f650628a18ec82b505d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "f5f9d50d5961f1446a57",
+    "url": "/jammming/static/js/runtime~main.85c7c486.js"
+  },
+  {
+    "revision": "58b4177a75991b512cf7",
+    "url": "/jammming/static/js/main.6b4ad74b.chunk.js"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/jammming/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "58b4177a75991b512cf7",
+    "url": "/jammming/static/css/main.aef30522.chunk.css"
+  },
+  {
+    "revision": "20645ffacdc1751a222be88600dfa022",
+    "url": "/jammming/index.html"
+  }
+];
